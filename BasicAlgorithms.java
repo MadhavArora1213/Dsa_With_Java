b79c37142d3887a16e9520791bd254614b56a215
Madhav Arora <madhavarora132005@gmail.com>
2024-08-28 19:59:37 +0530

    Add initial files

---
+++ BasicAlgorithms.java
@@ -0,0 +1,12 @@
+public class BasicAlgorithms{
+  static void func(float a,float b, float c){
+    float sum =a+b+c;
+    System.out.println("Sum of the given numbers: "+sum);
+    float avg = sum/3;
+    System.out.println("Average of the given numbers: "+avg);
+
+  }
+  public static void main(String[] args){
+    func(6,8,9);
+  }
+}
