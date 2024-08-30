0923b7aa3382c678313117d3777551c8338b41d8
Madhav Arora <madhavarora132005@gmail.com>
2024-08-30 06:24:46 +0530

    Day2

--- Calculate_Time_Complexity_Of_Linear_Algorithms.java
+++ Calculate_Time_Complexity_Of_Linear_Algorithms.java
@@ -1,5 +1,17 @@
 public class Calculate_Time_Complexity_Of_Linear_Algorithms {
     public static void main(String[] args) {
-        
+        Calculate_Time_Complexity_Of_Linear_Algorithms demo = new Calculate_Time_Complexity_Of_Linear_Algorithms();
+        System.out.println(demo.FindSum(3));
     }
+
+    //This Block of code takes n times to execute because of for loop that why its time complexity is O(n)--> The Linear Time Complexity.
+    
+    public int FindSum(int n){
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    
 }
