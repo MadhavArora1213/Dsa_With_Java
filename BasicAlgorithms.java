480d4c83229a24cfd0071c22535ece96af710fd8
Madhav Arora <madhavarora132005@gmail.com>
2024-08-30 05:52:03 +0530

    Day2

--- BasicAlgorithms.java
+++ BasicAlgorithms.java
@@ -1,12 +1,13 @@
 public class BasicAlgorithms{
-  static void func(float a,float b, float c){
-    float sum =a+b+c;
+  static void func(float a,float b, float c){    //Not use public keyword if you use it give an error
+    float sum =a + b + c;
     System.out.println("Sum of the given numbers: "+sum);
-    float avg = sum/3;
+    float avg = sum / 3;
     System.out.println("Average of the given numbers: "+avg);
-
   }
+
   public static void main(String[] args){
     func(6,8,9);
   }
+  
 }
---
+++ TimeComplexity.java
@@ -0,0 +1,26 @@
+
+public class TimeComplexity {
+
+    public static void main(String[] args) {
+        double now = System.currentTimeMillis();
+        TimeComplexity demo = new TimeComplexity();
+        System.out.println(demo.FindSum(99999));
+        System.out.println("Time Taken - " + (System.currentTimeMillis() - now) + "millisecs.");
+    }
+    
+    // O(n) Time Complexity - Linear Time Complexity   -> We can calculate the time complexity here its take 2 millisecs to run this code.
+
+    public int FindSum(int n){
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    
+    // O(1) Time Complexity - Constant Time Complexity -> We can calculate the time complexity here its take 1 millisecs to run this code.
+
+    // public int FindSum(int n){
+    //         return n*(n+1)/2;
+    //     }
+}
