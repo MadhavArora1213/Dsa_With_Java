06c7f7389f60d6bdbf38daed473cea3a9c222665
Madhav Arora <madhavarora132005@gmail.com>
2024-09-17 12:18:10 +0530

    Day10

---
+++ LeetCode_Array/Problem35.java
@@ -0,0 +1,28 @@
+// Search Insert Position
+package LeetCode_Array;
+
+public class Problem35 {
+    public static int searchInsert(int[] arr, int target) {
+        int st = 0;
+        int end = arr.length - 1;
+        int ans = arr.length; // Initialize ans to the end of the array
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            }
+            if (target < arr[mid]) {
+                end = mid - 1;
+                ans = mid; // Update ans to the current mid
+            } else {
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+       System.out.println(searchInsert(new int[] {2,5,6,7}, 5));
+    }
+}
---
+++ LeetCode_Array/Problem88.java
@@ -0,0 +1,32 @@
+// Merge Sorted Array
+
+package LeetCode_Array;
+
+public class Problem88 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Index of the last element in the merged array
+        int i = m + n - 1;
+        // Index of the last element in nums1's initialized part
+        int p1 = m - 1;
+        // Index of the last element in nums2
+        int p2 = n - 1;
+
+        // Merge the arrays from the end
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[i--] = nums1[p1--];
+            } else {
+                nums1[i--] = nums2[p2--];
+            }
+        }
+
+        // Copy remaining elements from nums2 (if any)
+        while (p2 >= 0) {
+            nums1[i--] = nums2[p2--];
+        }
+    
+    }
+    public static void main(String[] args) {
+  
+    }
+}
