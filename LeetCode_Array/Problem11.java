d96f9dd93db6ad06b60a7a27f6b36acdb737e490
Madhav Arora <madhavarora132005@gmail.com>
2024-10-08 06:09:40 +0530

    add

---
+++ LeetCode_Array/Problem11.java
@@ -0,0 +1,26 @@
+// Container with most water
+package LeetCode_Array;
+
+public class Problem11{
+    public int maxArea(int[] height) {
+        int MaxWater = 0;
+        int lp= 0, rp= height.length -1;
+        while(lp<rp){
+            int w = rp-lp;
+            int ht = Math.min(height[lp], height[rp]);
+            int area = w*ht;
+            MaxWater = Math.max(MaxWater,area);
+            if(height[lp]<height[rp]){
+                lp++;
+            }else{
+                rp--;
+            }
+                    
+                    
+        }
+        return MaxWater;
+    }
+    public static void main(String[] args){
+        
+    }
+}
